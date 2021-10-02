9ef4c7d835bf2a10468ac29bbd09fa07f17008d5
Vitalii Onopriienko <onoprienko.vit@gmail.com>
2021-10-02 10:40:37 +0300

    project.html

--- js/common.js
+++ js/common.js
@@ -2,10 +2,20 @@
 var rellax
 const rellaxStart = ()=>{
 	rellax = new Rellax('.rellax',{
-    	speed: 1.3
+    	speed: 1.3,
+    	center: false,
 	});
 }
 rellaxStart()
+
+var rellaxCenter
+const rellaxStartCenter = ()=>{
+	rellaxCenter = new Rellax('.rellax-center',{
+    	speed: 1.3,
+    	center: true,
+	});
+}
+rellaxStartCenter()
 
 
 
@@ -33,10 +43,25 @@ var bLazy = new Blazy({
 
 
 
+
+//mediumZoom
+const zoom = mediumZoom('[data-zoomable]',{
+	margin: 0,
+})
+zoom.on('close', (e) => {
+	document.querySelector(".medium-zoom-image--opened").classList.add("hide")
+	document.querySelector(".medium-zoom-image--hidden").classList.add("show")
+	document.querySelector(".medium-zoom-image--hidden").classList.remove("medium-zoom-image--hidden")
+}) 
+zoom.on('closed', (e) => {
+	document.querySelector(".show").classList.remove("show")	
+}) 
+
+
+
 //gsap smooth page scroll
 var html = document.documentElement;
 var body = document.body;
-
 var scroller = {
   target: document.querySelector("#scroll-container"),
   ease: 0.04, // <= scroll speed
@@ -45,57 +70,44 @@ var scroller = {
   resizeRequest: 1,
   scrollRequest: 0,
 };
-
 var requestId = null;
-
-TweenLite.set(scroller.target, {
-  rotation: 0.01,
-  force3D: true
-});
-
+// TweenLite.set(scroller.target, {
+//   rotation: 0.01,
+//   force3D: true
+// });
 window.addEventListener("load", onLoad);
-
 function onLoad() {    
   updateScroller();  
   window.focus();
   window.addEventListener("resize", onResize);
   document.addEventListener("scroll", onScroll); 
 }
-
 function updateScroller() {
-  
+	console.log("updateScroller")
   var resized = scroller.resizeRequest > 0;
-    
   if (resized) {    
     var height = scroller.target.clientHeight;
     body.style.height = height + "px";
     scroller.resizeRequest = 0;
   }
-      
   var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
   scroller.endY = scrollY;
   scroller.y += (scrollY - scroller.y) * scroller.ease;
-
   if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
     scroller.y = scrollY;
     scroller.scrollRequest = 0;
   }
-  
   TweenLite.set(scroller.target, { 
     y: -scroller.y 
   });
-  
   requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
 }
-
 function onScroll() {
   scroller.scrollRequest++;
   if (!requestId) {
     requestId = requestAnimationFrame(updateScroller);
   }
 }
-
 function onResize() {
   scroller.resizeRequest++;
   if (!requestId) {
@@ -137,7 +149,7 @@ function onResize() {
 		sortFilter()	
 
 		setTimeout(()=>{
-			bLazy.revalidate()
+			
        		AOS.refresh();
        		rellax.destroy();
        		rellaxStart()
@@ -149,6 +161,7 @@ function onResize() {
 	let filterElements = document.querySelectorAll('.projects-filter nav a');
 	for(let element of filterElements){
 		element.addEventListener("click", (e)=>{
+			bLazy.load(document.querySelectorAll('.section-projects .project-item img'))
 			for(let element of filterElements){
 				element.classList.remove("active")
 			}
@@ -157,3 +170,6 @@ function onResize() {
 		});
 	}
 })()
+
+
+
